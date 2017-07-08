1b6d3b033c6c6819bb7be9c8f42e9859f32455ea
Matt Spiewak <matthew.spiewak@gmail.com>
2017-07-07 21:02:06 -0400

    Added temp log and ignore files

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 from gpiozero import CPUTemperature
 from datetime import datetime
 import Adafruit_DHT as dht
+import os
+import sqlite3
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'roomtemp.db')
+))
+
+def connect_db():
+    """Connects to the spcific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    """Opens a new database connection if there is none yet 
+    for the current application context."""
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
 
 @app.route('/')
 def index():
@@ -24,11 +44,20 @@ def roomtemp():
 
 @app.route('/templog')
 def templog():
-    return render_template('templog.html')
+    db = get_db()
+    cur = db.execute('select temperature, humidity, timestamp from roomtemp order by timestamp desc')
+    temps = cur.fetchall()
+    return render_template('templog.html', temps=temps)
 
 @app.route('/hello/<name>')
 def hello(name):
     return render_template('page.html', name=name)
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the requtest"""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- logtemp.py
+++ logtemp.py
@@ -1,6 +1,13 @@
 from datetime import datetime
 import Adafruit_DHT as dht
 import sqlite3
+import os
+
+# Get path info
+db_file = 'roomtemp.db'
+curr_dir = os.path.abspath(os.path.dirname(__file__))
+full_db_path = os.path.join(curr_dir, db_file)
+
 
 # First capture time, humidity and temp to log in DB
 current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -10,7 +17,7 @@ temperature = temperature * 9/5.0 + 32
 print(current_time, format(temperature, '.2f'), format(humidity, '.2f'))
 
 # Open connection to DB and create cursor
-dbconn = sqlite3.connect('roomtemp.db')
+dbconn = sqlite3.connect(full_db_path)
 c = dbconn.cursor()
 c.execute('insert into roomtemp (temperature, humidity, timestamp) values (?, ?, ?);', (format(temperature, '.2f'), format(humidity, '.2f'), current_time))
 dbconn.commit()
